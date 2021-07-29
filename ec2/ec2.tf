82dbe078e308ab4ab79e4f98e29aef3d2e3466ff
Esin1942 <esinyurtdakal@gmail.com>
2021-07-29 16:44:56 +0000

    update ec2

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -2,9 +2,7 @@
 resource "aws_instance" "web" {
   ami                         = "ami-ae6272b8"
   instance_type               = "t2.micro"
-  availability_zone           = "us-east-1"
   associate_public_ip_address = true
   monitoring                  = true
-  key_name                    = aws_key_pair.laptop_key.key_name
   user_data                   = file("userdata.sh")
 }
