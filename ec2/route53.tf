b2b5cf5aeb3ff1c0531a11e80e129a1753d16de8
Esin1942 <esinyurtdakal@gmail.com>
2021-07-29 17:20:58 +0000

    edited

--- ec2/route53.tf
+++ ec2/route53.tf
@@ -1,3 +1,6 @@
-resource "aws_route53_zone" "wordpress" {
-  name = "wordpress.awsbasic.net"
-}
+resource "aws_route53_record" "www" {
+  zone_id = "Z00249131XYH0SZFZJZQN"
+  name    = "wordpress.awsbasic.net"
+  type    = "A"
+  ttl     = "300"
+   records = [aws_instance.wordpress.public_ip]
